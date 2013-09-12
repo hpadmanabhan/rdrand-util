e117d65d50ed0c5b637cd08dacdb74eecbcdbe89
Hemant Padmanabhan <padmanabhanhemant@gmail.com>
2013-09-12 00:33:51 +0000

    BZ 38:Applied license.

--- lizalab-rdrand-util/scripts/build-rdrand.sh
+++ lizalab-rdrand-util/scripts/build-rdrand.sh
@@ -1,8 +1,26 @@
+##
+# Copyright 2013 Hemant Padmanabhan
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
 #! /bin/bash
+#
+# build-rdrand.sh
 #
 # Builds Intel's RdRand Library and places generated artifacts
 # where the JNI native build expects to find them.
-#
+
 echo "[INFO] Current Working Directory:"
 pwd
 
